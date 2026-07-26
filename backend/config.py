83f529b896ee892f49e34538cc186da95c106480
bhumikaborkar7 <bhumikaborkar7@gmail.com>
2026-07-26 12:53:17 +0530

    Added folder creation in config.py

--- backend/config.py
+++ backend/config.py
@@ -8,39 +8,28 @@ Keeping them in one place makes the project easier to maintain.
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
 
-# Folder where uploaded PDFs are stored
-DOCUMENTS_FOLDER = "documents"
+load_dotenv()  # Load environment variables from .env file
 
-# Folder where ChromaDB stores embeddings
-CHROMA_DB_FOLDER = "chroma_db"
+DOCUMENTS_FOLDER = "documents"   # Folder where uploaded PDFs are stored
+CHROMA_DB_FOLDER = "chroma_db"   # Folder where ChromaDB stores embeddings
 
-# Read the Gemini API key from the .env file
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+os.makedirs(DOCUMENTS_FOLDER, exist_ok=True)   #creating folders if they do not exist
+os.makedirs(CHROMA_DB_FOLDER, exist_ok=True)
 
-# Gemini model to use
-GEMINI_MODEL = "gemini-2.5-flash"
-
-# Maximum allowed PDF upload size (20 MB)
-MAX_FILE_SIZE = 20 * 1024 * 1024
-
-# Allowed file extensions
-ALLOWED_EXTENSIONS = {".pdf"}
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")    # Read the Gemini API key from the .env file
+GEMINI_MODEL = "gemini-2.5-flash"     # Gemini model to use
+MAX_FILE_SIZE = 20 * 1024 * 1024     # Maximum allowed PDF upload size (20 MB)
+ALLOWED_EXTENSIONS = {".pdf"}    # Allowed file extensions
 
 # ==========================
 # RAG SETTINGS
 # ==========================
 
-# Number of search results to retrieve from ChromaDB
-TOP_K_RESULTS = 3
 
-# Number of characters in one chunk
-CHUNK_SIZE = 500
-
-# Number of overlapping characters between chunks
-CHUNK_OVERLAP = 100
+TOP_K_RESULTS = 3    #Number of search results to retrieve from ChromaDB
+CHUNK_SIZE = 500     #Number of characters in one chunk
+CHUNK_OVERLAP = 100     # Number of overlapping characters between chunks
 
 # ==========================
 # API SETTINGS
